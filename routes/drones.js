8b8e9c42c313e6671d18a8622ac90da8cfbda90e
Raul Nogales <rraulnnogales@gamil.com>
2021-07-18 14:07:15 +0200

    lab done

--- routes/drones.js
+++ routes/drones.js
@@ -2,35 +2,77 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require('../models/Drone.model');
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
+  Drone.find() //con Drone nos referimos a el model(plantilla) que encontramos en model.    .find() encuentra todos los objetos dentro de la base de datos
+  .then(allDrones => { //una vez tenemos los datos pasamos al siguiente paso
+    res.render('./drones/list', {allDrones}) //muestra la informacion en el archivo que he escrito. y mostramos la informacion de la base de datos
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  res.render('./drones/create-form')
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  const {name, propellers, maxSpeed} = req.body
+  Drone.create({name, propellers, maxSpeed})
+  .then(createDrone => {
+    console.log(createDrone)
+    res.redirect('/drones')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/drones/create')
+  })
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  Drone.findById(req.params.id)
+  .then(accessId => res.render('./drones/update-form', accessId))
+  .catch(err => console.log(err))
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  let id = req.params.id
+  const {name, propellers, maxSpeed} = req.body
+  Drone.findByIdAndUpdate(id, {name, propellers, maxSpeed})
+  .then(updateDroneById => {
+    console.log(updateDroneById)
+    res.redirect('/drones')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect(`/drones/${id}/edit`)
+  })
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
+  Drone.findByIdAndDelete(req.params.id)
+  .then(deletedDrone => {
+    console.log(deletedDrone)
+    res.redirect('/drones')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/drones')
+  })
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,45 @@
+// 1: requerir los paquetes necesatios: mongoose, drone...
+// 2: crear los datos {array de datos que hay que almacenar en la base de datos}
+// 3: conectarnos a la base de datos: require(con el enlace al archivo de la base de datos)
+// 4: una vez nos hemos conectado a la vase de datos creamos cosas dentro
+
+
 // Iteration #1
+
+const mongoose = require("mongoose");
+
+const Drone = require('../models/Drone.model')
+
+const drone = [
+  {
+    name: 'DJI Phantom 4 Pro',
+    propellers: 4,
+    maxSpeed: 20
+  },
+  {
+    name: 'DJI INSPIRE 2',
+    propellers: 4,
+    maxSpeed: 26
+  },
+  {
+    name: 'Courier 3000i',
+    propellers: 6,
+    maxSpeed: 18
+  },
+];
+
+require('../db/index')
+
+Drone.deleteMany()
+.then(allDrones => {
+  console.log('everything deleted')
+})
+.then(
+Drone.create(drone)
+.then((createdDrones) => {
+  console.log('drones created in database')
+  mongoose.connection.close();
+})
+.catch((err) => {
+  console.log(err)
+}))
